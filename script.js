c9e8db9d9d7c6e52a3169e77549850237608dafa
koach-noir <noe.k@ymail.ne.jp>
2024-08-01 16:42:51 +0900

    Update multiple articles and content

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const gameContainer = document.getElementById('game-container');
+// const gameContainer = document.getElementById('game-container');
 
-function startGame() {
-    for (let i = 0; i < balloonCount; i++) {
-        createBalloon();
-    }
-}
+// function startGame() {
+//     for (let i = 0; i < balloonCount; i++) {
+//         createBalloon();
+//     }
+// }
 
-startGame();
+// startGame();
